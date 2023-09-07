e88f4c623003b44f04a89f88c24a611a66dbc4bd
120-cell <120.cheshire@gmail.com>
2023-09-07 23:53:48 +0200

    fixed working directory on windows

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ from setting_handler import Setting_Handler, SETTINGS_PATH
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
+    
+    abspath = os.path.abspath(__file__)
+    dirname = os.path.dirname(abspath)
+    os.chdir(dirname)
     if os.path.isfile(SETTINGS_PATH):
         logging.info('loading settings from file')
         with open(SETTINGS_PATH, 'rb') as file:
