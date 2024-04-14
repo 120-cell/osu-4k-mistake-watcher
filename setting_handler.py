8066693373bbc24b6999085211344103a3de322f
120-cell <120.cheshire@gmail.com>
2024-04-14 15:30:41 +0200

    set default bind scan codes

--- setting_handler.py
+++ setting_handler.py
@@ -9,7 +9,7 @@ class Setting_Handler():
     def __init__(self):
         self.KEYS = 4
         self.bind_names = ['s', 'd', 'f', 'space', '`']
-        self.bind_codes = [0, 0, 0, 0, 0]
+        self.bind_codes = [31, 32, 33, 57, 41]
         self.colours = ['#DC0014', '#FF8C0A', '#00C1C1', '#2832E6']
         self.aliases = ['ring', 'middle', 'index', 'thumb']
         
