a80adbd37c6c51a6c05663ece07dd9ab8fbd1cda
120-cell <120.cheshire@gmail.com>
2024-04-15 15:44:09 +0200

    cleanup

--- app.py
+++ app.py
@@ -305,23 +305,3 @@ class App(tk.Tk):
     def on_close(self):
         self.settings.save()
         self.destroy()
-        
-
-
-def modular_range(modulus, start, end):
-    start = start % modulus
-    end = end % modulus
-    if start <= end:
-        return range(start, end)
-    return chain(range(start, modulus), range(end))
-
-
-def is_hexcode(code):
-    if not isinstance(code, str):
-        logging.info('hexcode input is not a string')
-        return False
-    match = re.fullmatch(r'#?[0-9a-fA-F]{6}', code)
-    if match:
-        return True
-    logging.info('hexcode string is invalid')
-    return False
