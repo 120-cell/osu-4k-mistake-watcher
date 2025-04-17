51253bbcd5a933b184b181dc7f7307b329309ff1
120-cell <120.cheshire@gmail.com>
2025-04-17 19:02:48 +0200

    changed default keybinds

--- setting_handler.py
+++ setting_handler.py
@@ -7,12 +7,14 @@ SETTINGS_PATH = 'settings.p'
 
 class Setting_Handler():
     def __init__(self):
+        # keys
         self.KEYS = 4
-        self.bind_names = ['s', 'd', 'f', 'space', '`']
+        self.bind_names = ['a', 's', 'd', 'space', '`']
         self.bind_codes = [31, 32, 33, 57, 41]
         self.colours = ['#DC0014', '#FF8C0A', '#00C1C1', '#2832E6']
         self.aliases = ['ring', 'middle', 'index', 'thumb']
         
+        # display options
         self.key_display_method = 'key numbers'
         self.font_size = 18
         self.relative_pad_left = 0.5
